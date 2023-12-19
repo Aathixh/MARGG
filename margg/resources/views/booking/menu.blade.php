b05552bf3edce9928ed002e99dc4c4e8d9a656a2
RottenSpaceMan <65809047+RottenSpaceMan@users.noreply.github.com>
2023-12-19 19:46:17 +0530

    table push

--- margg/resources/views/booking/menu.blade.php
+++ margg/resources/views/booking/menu.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/menustyles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>book Tickets</title>
 </head>
 
@@ -19,7 +20,7 @@
     <!-- here this /locate url is given to the book now button for the tym being... in real program it must redirect somewhere else like payment gateway.... -->
 
 
-    <table border="1">
+    <table border="1" class="table">
         <tr>
             <th>Sl.No</th>
             <th>Bus_No</th>
@@ -44,7 +45,14 @@
             <td>{{$data->Arrival_Time}}</td>
             <td>{{$data->Price}}</td>
             <td style="text-align: center;"><a href="/locate_new">
-                    <button>
+                    <button style="border: none;
+                    background: none;
+                    padding: 0;
+                    margin: 0;
+                    font-size: inherit;
+                    font-family: inherit;
+                    color: inherit;
+                    cursor: pointer;">
                         <div class="text">Book Now</div>
                     </button>
                 </a>
@@ -54,5 +62,5 @@
     </table>
 
 </body>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </html>
