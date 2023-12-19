914d7b77f2ecf655680dfa307ca3e3573f4dfd1a
Aathish R Viswam <aathishrv@gmail.com>
2023-12-19 10:04:36 +0530

    Update home.blade.php

--- margg/resources/views/home.blade.php
+++ margg/resources/views/home.blade.php
@@ -8,7 +8,19 @@
 </head>
 
 <body>
+    <header>
+        <h1>Welcome to Margg!</h1>
+    </header>
+    <a href="/login"><button>Register</button></a>
+    <a href="/signin"><button>Login</button></a>
+    <footer>
+        <h3>About</h3>
+        <p>"MARGG" is a startup idea focused on revolutionizing the way people track buses and enhance public
+            transportation systems. Here's a brief overview of what MARGG could offer:
 
+            Concept: MARGG is a comprehensive bus tracking and management system designed to provide real-time
+            information to both passengers and transportation authorities.</p>
+    </footer>
 </body>
 
 </html>
