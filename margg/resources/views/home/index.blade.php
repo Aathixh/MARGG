d1d27e332cf00459e23753946643d51f83e899ac
Aathish R Viswam <aathishrv@gmail.com>
2023-12-20 17:01:19 +0530

    some polish

--- margg/resources/views/home/index.blade.php
+++ margg/resources/views/home/index.blade.php
@@ -12,19 +12,28 @@
     <div class="content">
         <header class="top-bar">
             <nav><a href="#about">ABOUT</a></nav>
+            <form>
+                <label for="lang">Language:</label>
+                <select name="cars" id="cars">
+                    <option value="volvo">English</option>
+                    <option value="saab">Hindi</option>
+                    <option value="opel">Tamil</option>
+                    <option value="audi">Malayalam</option>
+                </select>
+            </form>
         </header>
         <div class="head">
-        <img src="img/black.png" alt="logo">
-        <div class="container">
-            <button onclick="window.location.href='/login'"><span class="shadow"></span>
-                <span class="edge"></span>
-                <span class="front text">Register
-                </span></button>
-            <button onclick="window.location.href='/signin'"><span class="shadow"></span>
-                <span class="edge"></span>
-                <span class="front text">Login
-                </span></span></button>
-        </div>
+            <img src="img/black.png" alt="logo">
+            <div class="container">
+                <button onclick="window.location.href='/login'"><span class="shadow"></span>
+                    <span class="edge"></span>
+                    <span class="front text">Register
+                    </span></button>
+                <button onclick="window.location.href='/signin'"><span class="shadow"></span>
+                    <span class="edge"></span>
+                    <span class="front text">Login
+                    </span></span></button>
+            </div>
         </div>
         <footer id="about">
             <h3>ABOUT</h3>
@@ -33,7 +42,8 @@
                 transportation systems. <br>Here's a brief overview of what MARGG could offer:
 
                 <br>Concept: MARGG is a comprehensive bus tracking and management system designed to provide real-time
-                information to both passengers and transportation authorities.</p>
+                information to both passengers and transportation authorities.
+            </p>
         </footer>
     </div>
 </body>
--- margg/resources/views/locate/alert.blade.php
+++ margg/resources/views/locate/alert.blade.php
@@ -9,18 +9,20 @@
 </head>
 
 <body>
-    <form action="/alertmsg" method="post">
-        @csrf
-        @method('post')
-        <input type="text" name="messages">
-        <button type="submit">Submit</button>
-    </form>
 
-    @if(session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
-    </div>
-    @endif
+    <form action="/alertmsg" method="post">
+        <div class="input-group">
+            @csrf
+            @method('post')
+            <input type="text" name="messages" autocomplete="off" class="input">
+            <input class="button--submit" value="Alert" type="submit">
+        </div>
+        @if(session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+        @endif
+    </form>
 </body>
 
 </html>
