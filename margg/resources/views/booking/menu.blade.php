bcc241c574f9d0113f5fb977bdbf0ad9bdacd479
RottenSpaceMan <65809047+RottenSpaceMan@users.noreply.github.com>
2023-12-20 08:31:32 +0530

    bootstrap strip

--- margg/resources/views/booking/menu.blade.php
+++ margg/resources/views/booking/menu.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/menustyles.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    
     <title>book Tickets</title>
 </head>
 
@@ -18,7 +18,6 @@
     </a>
 </div>
     <!-- here this /locate url is given to the book now button for the tym being... in real program it must redirect somewhere else like payment gateway.... -->
-
 
     <table border="1" class="table">
         <tr>
@@ -59,8 +58,9 @@
             </td>
         </tr>
         @endforeach
+        
     </table>
 
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
 </html>
